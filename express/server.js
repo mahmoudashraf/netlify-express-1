f6598796203621ae85e80863fb4b51098baeb492
Mahmoud Ashraf Algammal <engmahmoudalgamal@gmail.com>
2022-09-06 18:34:09 +0200

    yft

--- express/server.js
+++ express/server.js
@@ -6,33 +6,17 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-
-let userBalance = 0
-app.use(express.json())
-
-
-app.get("/api/balance", (req, res) => {
-  res.status(200).send({ balance: userBalance })
-})
-app.post("/api/balance", (req, res) => {
-  const { amount } = req.body
-  if (typeof amount !== "number" && amount > 0) return res.status(400).send({ error: "Invalid amount!" })
-  userBalance += amount
-  res.status(200).send()
-})
-
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/api/balance', (req, res) =>  res.status(200).send({ balance: userBalance }));
-router.post('/api/balance', (req, res) =>  {const { amount } = req.body
-if (typeof amount !== "number" && amount > 0) return res.status(400).send({ error: "Invalid amount!" })
-userBalance += amount
-res.status(200).send()
-});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
